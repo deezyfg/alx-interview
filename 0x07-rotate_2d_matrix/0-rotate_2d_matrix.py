6b5dad7f06109552de08af2722b9aea2a289a6c3
deezyfg <mensahpeter421@gmail.com>
2024-09-10 05:54:03 +0000

    Added all files

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module for rotating a 2D matrix 90 degrees clockwise.
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    Rotate an n x n 2D matrix 90 degrees clockwise in-place.
+
+    Args:
+    matrix (List[List[int]]): The input matrix to be rotated.
+
+    Returns:
+    None. The matrix is modified in-place.
+    """
+    # Get the size of the matrix
+    n = len(matrix)
+
+    # Transpose the matrix
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # Reverse each row
+    for i in range(n):
+        matrix[i].reverse()
---
+++ 0x07-rotate_2d_matrix/README.md
@@ -0,0 +1,6 @@
+0x07. Rotate 2D Matrix
+===================================
+
+> Algorithm | Python
+
+This project focuses on implementing an algorithm to rotate a 2D matrix (or a square matrix) by 90 degrees clockwise in-place using Python. The task involves manipulating the elements of a square matrix without using any additional data structures, effectively transforming the original matrix through a series of swaps or rotations to achieve the desired 90-degree clockwise rotation.
